51dae9ef23f0282d889ebb91f59e35c44ead551b
Fran <user@users-MacBook-Pro.local>
2024-08-19 13:09:15 +0200

    message_exit documented

--- src/error.c
+++ src/error.c
@@ -1,5 +1,10 @@
 #include <minirt.h>
 
+// función que muestra un mensaje por pantalla y sale de forma exitosa
+// Esta función está bien pero hay que cambiarla para que se le pase como 
+// argumento un puntero a t_world y un puntero a t_server, para llamar a las
+// funciones que liberan dichos punteros.
+// Si se le pasa NULL como argumento no liberarán dichos punteros
 void	message_exit(char *msg)
 {
 	if (msg)
