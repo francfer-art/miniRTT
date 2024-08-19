fd4735a37d11494050e9190ab47b3de7185fc0ea
Fran <user@users-MacBook-Pro.local>
2024-08-19 17:27:59 +0200

    Spheres documented

--- src/sphere.c
+++ src/sphere.c
@@ -1,5 +1,11 @@
 #include <minirt.h>
 
+// Función que crea y rellena una esfera y devuelve un puntero a dicha estructura
+// Lo primero que hacemos es comprobar la longitud del puntero doble, si la 
+// longitud es distinta de 4 mostramos un error devolvemos NULL.
+// Ahora alojamos dinámicamente un puntero a dicha estructura, y empezamos a
+// rellenar la estructura.
+// Finalmente devolvemos el puntero a dicha variable
 t_sphere	*new_sphere(char **data)
 {
 	t_sphere *sphere;
@@ -16,6 +22,14 @@ t_sphere	*new_sphere(char **data)
 	return (sphere);
 }
 
+// Función para calcular las raíces que se generan entre una esfera y un rayo
+// Recibe de argumentos el rayo, la esfera y un array de floats, donde se almacenarán
+// las raíces
+// Lo primero que hacemos es calcular oc, es un vector que representa la distancia
+// y dirección del centro de la esfera al origen del rayo
+// a, half-b y c son los coeficientes de la escuación cuadrática
+// Calculamos el discriminante de la ecuación cuadrática y procedemos a calcular
+// las dos raíces
 void	sphere_roots(t_ray ray, t_sphere sphere, float *root)
 {
 	t_vector	oc;
@@ -33,6 +47,16 @@ void	sphere_roots(t_ray ray, t_sphere sphere, float *root)
 	root[1] = (-half_b - sqrt_disc) / a;
 }
 
+// Función que determina si un rayo ha interseccionado con una esfera, devuelve
+// 1 si hay intersección y 0 si no la hay.
+// Lo primero que hacemos es calcular las raíces, llamando a sphere_roots
+// La función verifica si la raíz actual root[i] es un valor positivo 
+// (lo que significa que la intersección está delante del origen del rayo, no detrás)
+// y si es menor que ray->record.t (lo que indica que es la intersección más cercana 
+// al origen del rayo que se ha encontrado hasta ahora).
+// Actualizamos los registros del rayo y devolvemos 1. En caso de que la raíz
+// no sea válida iteraremos por el bucle. Si no se ha devulto 1 en ninguna vuelta, 
+// devolvemos 0.
 int	hit_sphere(t_ray *ray, t_sphere *sphere)
 {
 	unsigned int	i;
